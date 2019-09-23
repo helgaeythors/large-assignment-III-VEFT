0a09ec33db6879235613c14d62c7af4e6eea6402
unknown <helga.ey@gmail.com>
2019-09-23 16:34:19 +0000

    adding express

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Here the web service should be setup and routes declared
+// Here the web service should is setup and routes declared
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+   console.log("Server is listening on port 3000");
+});
